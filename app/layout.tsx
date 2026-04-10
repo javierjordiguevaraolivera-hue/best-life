bdf4722e51b972b7f351f05a31c3a83ced88b4bc
Antony <javierjordiguevaraolivera@gmail.com>
2026-04-10 10:56:12 -0500

    taboola

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import Script from "next/script";
+import PixelScripts from "./pixel-scripts";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,52 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Script id="meta-pixel-base" strategy="beforeInteractive">
-          {`
-            if (window.location.pathname !== '/tab1') {
-              var __metaPixelId = window.location.pathname === '/iul-3560-mc'
-                ? '1532513301232175'
-                : window.location.pathname === '/iul-en-qt'
-                  ? '4183928465176236'
-                  : '980723860687387';
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', __metaPixelId);
-              fbq('track', 'PageView');
-            }
-          `}
-        </Script>
-        <Script id="tiktok-pixel-base" strategy="beforeInteractive">
-          {`
-            if (window.location.pathname !== '/tab1') {
-              !function (w, d, t) {
-                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];
-                ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
-                ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
-                ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};
-                ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");
-                n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
-                ttq.load('D35C49RC77U1VDRE0SV0');
-                ttq.page();
-              }(window, document, 'ttq');
-            }
-          `}
-        </Script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=980723860687387&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
+        <PixelScripts />
         {children}
       </body>
     </html>
