b8e510be5269a0b4f8fa090b74910956ddf0576f
Antonymm225 <amachuca330@gmail.com>
2026-08-19 08:55:35 -0500

    recoger ad id de utm_term

--- app/iul-v4/page.tsx
+++ app/iul-v4/page.tsx
@@ -1559,7 +1559,7 @@ export default function Home() {
       const sub1 = urlParams.get("sub1")?.trim() || "";
       const sub2 = urlParams.get("sub2")?.trim() || "";
       const adaccountName = urlParams.get("adaccount_name")?.trim() || "";
-      const adId = urlParams.get("ad_id")?.trim() || urlParams.get("utm_term")?.trim() || "";
+      const adId = urlParams.get("ad_id")?.trim() || urlParams.get("utm_content")?.trim() || "";
       const cleanedAnswers = Object.fromEntries(
         Object.entries({
           ageGroup: completedAnswers.ageGroup,
